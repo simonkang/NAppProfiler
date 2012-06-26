01624e824b044e6932e474a70126f0234f9f1b9e
Simon Kang <simon.kang@gmail.com>
2012-06-26 11:29:33 +0900

    Search Error

--- NAppProfiler.Web/Controllers/SearchController.cs
+++ NAppProfiler.Web/Controllers/SearchController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using NAppProfiler.Client;
 using NAppProfiler.Client.DTO;
+using NAppProfiler.Web.Models;
+using System.Globalization;
 
 namespace NAppProfiler.Web.Controllers
 {
@@ -12,35 +14,63 @@ namespace NAppProfiler.Web.Controllers
     {
         //
         // GET: /Search/
-        public void IndexAsync(NAppProfiler.Web.Models.SearchRequest request)
+        public void IndexAsync(SearchRequest request)
         {
-            AsyncManager.OutstandingOperations.Increment();
-            AsyncManager.Parameters["offSetX"] = request.offSetX;
-            AsyncManager.Parameters["offSetY"] = request.offSetY;
-            AsyncManager.Parameters["canvasWidth"] = request.canvasWidth;
-            AsyncManager.Parameters["canvasHeight"] = request.canvasHeight;
-            var req = new LogQuery()
+            var validReq = ValidateSearchRequest(request);
+            if (string.IsNullOrWhiteSpace(validReq.Item2))
             {
-                DateTime_From = DateTime.MinValue.AddYears(1),
-                DateTime_To = DateTime.MaxValue,
-            };
-            NAppProfilerClient.SendQuery(req, AsyncManager);
+                AsyncManager.OutstandingOperations.Increment();
+                AsyncManager.Parameters["offSetX"] = request.offSetX;
+                AsyncManager.Parameters["offSetY"] = request.offSetY;
+                AsyncManager.Parameters["canvasWidth"] = request.canvasWidth;
+                AsyncManager.Parameters["canvasHeight"] = request.canvasHeight;
+                NAppProfilerClient.SendQuery(validReq.Item1, AsyncManager);
+            }
+            else
+            {
+                AsyncManager.Parameters["errMsg"] = validReq.Item2;
+            }
         }
 
-        public ActionResult IndexCompleted(int offSetX, int offSetY, int canvasWidth, int canvasHeight, IList<LogQueryResultDetail> details)
+        public ActionResult IndexCompleted(int offSetX, int offSetY, int canvasWidth, int canvasHeight, string errMsg, IList<LogQueryResultDetail> details)
         {
-            var list = new List<int[]>();
-            //list.Add(new int[] { offSetX, offSetY, offSetX, offSetY + canvasHeight, 255, 0, 0 });
-            //list.Add(new int[] { offSetX, offSetY + canvasHeight, offSetX + canvasWidth, offSetY + canvasHeight, 255, 0, 0 });
-            //list.Add(new int[] { offSetX + canvasWidth, offSetY + canvasHeight, offSetX + canvasWidth, offSetY, 255, 0, 0 });
-            //list.Add(new int[] { offSetX + canvasWidth, offSetY, offSetX, offSetY, 255, 0, 0 });
+            if (string.IsNullOrWhiteSpace(errMsg))
+            {
+                var list = new List<int[]>();
+                //list.Add(new int[] { offSetX, offSetY, offSetX, offSetY + canvasHeight, 255, 0, 0 });
+                //list.Add(new int[] { offSetX, offSetY + canvasHeight, offSetX + canvasWidth, offSetY + canvasHeight, 255, 0, 0 });
+                //list.Add(new int[] { offSetX + canvasWidth, offSetY + canvasHeight, offSetX + canvasWidth, offSetY, 255, 0, 0 });
+                //list.Add(new int[] { offSetX + canvasWidth, offSetY, offSetX, offSetY, 255, 0, 0 });
 
-            var curDT = DateTime.MinValue;
-            var curElapsed = -1L;
-            foreach (var cur in details.AsParallel().OrderBy(i => i.Elapsed).ThenBy(i => i.CreatedDateTime))
+                var curDT = DateTime.MinValue;
+                var curElapsed = -1L;
+                foreach (var cur in details.AsParallel().OrderBy(i => i.Elapsed).ThenBy(i => i.CreatedDateTime))
+                {
+                }
+                return Json(new { points = list, errmsg = "" }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { errMsg = errMsg }, JsonRequestBehavior.AllowGet);
+        }
+
+        private Tuple<LogQuery, string> ValidateSearchRequest(SearchRequest request)
+        {
+            var req = new LogQuery();
+            var errMsg = string.Empty;
+            DateTime tempTime;
+            if (DateTime.TryParseExact(request.fromDate, "yy-MM-dd HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.AssumeLocal, out tempTime))
+            {
+                req.DateTime_From = tempTime;
+            }
+            else
             {
             }
-            return Json(new { points = list, errmsg = "" }, JsonRequestBehavior.AllowGet);
+            return new Tuple<LogQuery, string>(req, errMsg);
         }
+
+        private DateTime? ParseDateTime(string dt)
+        {
+            DateTime temp;
+            if (DateTime.TryParseExact(dt, "yy-MM-dd HH:mm", 
+       }
     }
 }
